cdb7523c1c121ae6334beb227a4ade8d7cfd012e
NTT102 <53190789+NTT102@users.noreply.github.com>
2025-09-07 21:42:56 +0700

    Update Hero.tsx

--- src/layouts/sections/Hero.tsx
+++ src/layouts/sections/Hero.tsx
@@ -4,13 +4,10 @@ const Hero = () => {
             <div className="w-full h-full flex items-center bg-slate-800 opacity-80">
                 <div className="max-w-[60rem] mx-auto p-4 grid gap-8">
                     <h2 className="uppercase text-5xl text-yellow-300 font-bold">
-                        Marketing Online
+                        EC MARKET
                     </h2>
                      <p className="text-white font-medium first-letter:uppercase">
-                        Với hơn 10 năm kinh nghiệm trong lĩnh vực MMO, Tuy sẽ không theo lý thuyết hay sách vở gì cả nhưng những cái gì e chia sẻ ở đây là những mẹo mà e đúc kết lại được để chia sẻ cho mọi người có 1 cái nhìn tổng quan cũng như có kinh nghiệm về MKT đa nền tảng để chiến được mọi ngành nghề lĩnh vực khác nhau !! 
-                    </p>
-                    <p className="text-white font-medium first-letter:uppercase">
-                         ** Khi tham gia cuộc chơi nào đó, bạn phải hiểu rõ luật chơi , sau đó hãy làm sao để là người chơi tốt nhất **
+                        EC Markets là một trong những sàn giao dịch quốc tế uy tín, được cấp phép bởi nhiều cơ quan tài chính lớn, mang đến môi trường minh bạch và an toàn cho nhà đầu tư. Với spread thấp từ 0.0 pips, tốc độ khớp lệnh cực nhanh chỉ vài mili-giây, nền tảng MT4/MT5 hiện đại cùng hơn 100 sản phẩm giao dịch đa dạng (Forex, vàng, dầu, chỉ số, crypto…), EC Markets phù hợp cho cả người mới bắt đầu lẫn trader chuyên nghiệp. Đây là lựa chọn đáng tin cậy cho những ai tìm kiếm sự ổn định, bảo mật và hiệu quả trong từng giao dịch. !! 
                     </p>
                 </div>
             </div>
