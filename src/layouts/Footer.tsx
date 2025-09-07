e6ed2cb953fa62f34ccb40543015dc1d24807aed
NTT102 <53190789+NTT102@users.noreply.github.com>
2025-09-07 21:51:08 +0700

    Update Footer.tsx

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,8 +1,19 @@
-import Logo from "../assets/logo.jpg";
+
 
 const Footer = () => {
     return (
         <footer className="bg-slate-800 text-white px-4 py-8">
+             <div className="max-w-[60rem] mx-auto">
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-2 border-b border-slate-500 pb-6">
+                    <div className="w-[8.625rem]">
+                        <img
+                            className="w-full h-auto object-cover"
+                            src={Logo}
+                            alt="Logo"
+                        />
+                    </div>
+                    
+                </div>
                 <p className="text-center font-medium text-slate-500 mt-6">
                     &copy; Allrights reserved Legend Group
                 </p>
