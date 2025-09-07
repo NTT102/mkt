7765c565159bd29a0ecdabc3bb8f0c520d2f3c45
NTT102 <53190789+NTT102@users.noreply.github.com>
2025-09-07 21:47:18 +0700

    Update CourseTabs.tsx

--- src/components/CourseTabs.tsx
+++ src/components/CourseTabs.tsx
@@ -4,6 +4,16 @@ const CourseTabs = () => {
     return (
         <div>
             <Tab.List className="w-full flex gap-2 sm:gap-4 justify-center items-center flex-wrap">
+                 <Tab
+                    className={({ selected }) => {
+                        if (selected)
+                            return "text-white text-lg font-semibold border-b border-white outline-none first-letter:uppercase py-2";
+
+                        return "text-gray-400 text-lg font-semibold border-b border-transparent outline-none first-letter:uppercase py-2";
+                    }}
+                >
+                    Nhập môn
+                </Tab>
                 <Tab
                     className={({ selected }) => {
                         if (selected)
